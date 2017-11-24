9e9dd2a09973ad0952037e72ac0dcae7ab66d2cb
pragion345 <jms100300@naver.com>
2017-11-24 21:33:40 +0900

    WordBundle Filed  수정

--- app/src/main/java/com/example/jms10/langstudykbrd/BaseLibrary/DataFromNet/DictionaryData/WordBundle.java
+++ app/src/main/java/com/example/jms10/langstudykbrd/BaseLibrary/DataFromNet/DictionaryData/WordBundle.java
@@ -1,11 +1,13 @@
 package com.example.jms10.langstudykbrd.BaseLibrary.DataFromNet.DictionaryData;
 
+import java.util.Dictionary;
+
 /**
  * Created by jms10 on 2017-11-24.
  */
 
 public class WordBundle {
-    WordData[] datas;
+    Dictionary<Integer, WordData> wordDataDictionary;
 
 
 }
