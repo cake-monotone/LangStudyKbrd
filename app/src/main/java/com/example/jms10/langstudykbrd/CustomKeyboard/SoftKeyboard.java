1bc7a1d748dd2a7197c0efca0fc27715a31a7ff9
Grapefruitsaft <und37d0g@gmail.com>
2017-12-09 19:47:26 +0900

    실시간  번역 (Threaed로 이용)

--- app/src/main/java/com/example/jms10/langstudykbrd/CustomKeyboard/SoftKeyboard.java
+++ app/src/main/java/com/example/jms10/langstudykbrd/CustomKeyboard/SoftKeyboard.java
@@ -3,7 +3,6 @@ package com.example.jms10.langstudykbrd.CustomKeyboard;
 import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
-import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -13,7 +12,8 @@ import android.widget.LinearLayout;
 
 import com.example.jms10.langstudykbrd.R;
 import com.example.jms10.langstudykbrd.SharedPreferenceUtil;
-import com.example.jms10.langstudykbrd.Translation.Translate;
+
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -21,7 +21,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Sojeong Jin on 30/11/2017.
@@ -41,6 +40,8 @@ public class SoftKeyboard extends InputMethodService implements KeyboardView.OnK
     private LangKeyboard SymbolShiftKeyboard;
     private LangKeyboard t_SymbolShiftKeyboard;
     private LangKeyboard TranslationKeyboard;
+
+    String tent = "";
 
     private final int[] ExceptedKeyArray = {};
     /*private final HashSet<Integer> ExceptedKeySet = new HashSet<>() {
@@ -89,6 +90,7 @@ public class SoftKeyboard extends InputMethodService implements KeyboardView.OnK
 
         Log.d("Test", "KEYCODE: "+primaryCode);
         InputConnection ic = edit_k.onCreateInputConnection(new EditorInfo());
+        String bt ="";
         getCurrentInputConnection().setSelection(0, prevCurrentlength);
         Keyboard current;
         switch(primaryCode){
@@ -97,104 +99,77 @@ public class SoftKeyboard extends InputMethodService implements KeyboardView.OnK
                     ic = getCurrentInputConnection();
                 }
                 ic.deleteSurroundingText(1, 0);
-                if(mInputView.getKeyboard() == TranslationKeyboard){
-                    if(mInputView.getKeyboard() == TranslationKeyboard) {
-                        prevtextlength = edit_k.getText().length();
-                        String bt = edit_k.getText().toString();
-                        Translate.Translation t = new Translate.Translation();
-                        try {
-                            String res = t.execute(bt).get();
-                            getCurrentInputConnection().commitText(res.toString(), 1);
-                            getCurrentInputConnection().setSelection(0, res.length());
-                            prevCurrentlength = res.length();
-                        }
-                        catch(InterruptedException e){
-                            e.printStackTrace();
-                        }
-                        catch(ExecutionException e){
-                            e.printStackTrace();
-                        }
-                        //t.handle
-                    }
-                }
-                break;
-            case Keyboard.KEYCODE_SHIFT:
-                current = mInputView.getKeyboard();
-                if(current == QwertyKeyboard|| current == TranslationKeyboard) {
-                    caps = !caps;
-                    mInputView.setShifted(caps);
-                }
-                else{
-                    if(current == SymbolKeyboard){
-                        mInputView.setKeyboard(SymbolShiftKeyboard);
-                    }
-                    else{
-                        mInputView.setKeyboard(SymbolKeyboard);
-                    }
-                }
-                /*Intent intent = new Intent(getApplicationContext(), TmpActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);*/
-
-                break;
-            case Keyboard.KEYCODE_DONE:
-                break;
-            case KEYCODE_DICTIONARY:
-                SharedPreferenceUtil sharedPreferenceUtil = new SharedPreferenceUtil(getApplicationContext());
-
-                break;
-            case KEYCODE_TRANSLATION:
-                trans_flag = !trans_flag;
-                if(!trans_flag){
-
-                   mInputView.setKeyboard(QwertyKeyboard);
-                }
-                else{
-                    mInputView.setKeyboard(TranslationKeyboard);
-                }
-
-                break;
-            case Keyboard.KEYCODE_MODE_CHANGE:
-                current = mInputView.getKeyboard();
-                if(current == TranslationKeyboard || current == t_SymbolKeyboard){
-                    mInputView.setKeyboard(t_SymbolShiftKeyboard);
-                }
-                else if(current == t_SymbolShiftKeyboard){
-                    mInputView.setKeyboard(t_SymbolKeyboard);
-                }
-                else if(current == QwertyKeyboard || current == SymbolKeyboard){
-                    mInputView.setKeyboard(SymbolShiftKeyboard);
-                }
-                else{
-                    mInputView.setKeyboard(SymbolKeyboard);
-                }
-
-                break;
-            default:
-                char code = (char)primaryCode;
-                if(Character.isLetter(code) && caps){
-                    code = Character.toUpperCase(code);
-                }
-                ic.commitText(String.valueOf(code),1);
-
                 if(mInputView.getKeyboard() == TranslationKeyboard) {
                     prevtextlength = edit_k.getText().length();
-                    String bt = edit_k.getText().toString();
-                    Translate.Translation t = new Translate.Translation();
-                    try {
-                        String res = t.execute(bt).get();
-                        getCurrentInputConnection().commitText(res.toString(), 1);
-                        getCurrentInputConnection().setSelection(0, res.length());
-                        prevCurrentlength = res.length();
+                    bt = edit_k.getText().toString();
+                    if(bt.length() != 0) {
+                        handletrasn(bt);
                     }
-                    catch(InterruptedException e){
-                        e.printStackTrace();
+                    else{
+                        getCurrentInputConnection().commitText("", 1);
                     }
-                    catch(ExecutionException e){
-                        e.printStackTrace();
-                    }
-                    //t.handle
                 }
+                    break;
+                    case Keyboard.KEYCODE_SHIFT:
+                        current = mInputView.getKeyboard();
+                        if (current == QwertyKeyboard || current == TranslationKeyboard) {
+                            caps = !caps;
+                            mInputView.setShifted(caps);
+                        } else {
+                            if (current == SymbolKeyboard) {
+                                mInputView.setKeyboard(SymbolShiftKeyboard);
+                            } else if (current == SymbolShiftKeyboard) {
+                                mInputView.setKeyboard(SymbolKeyboard);
+                            } else if (current == t_SymbolKeyboard) {
+                                mInputView.setKeyboard(t_SymbolShiftKeyboard);
+                            } else {
+                                mInputView.setKeyboard(t_SymbolKeyboard);
+                            }
+                        }
+
+                        break;
+                    case Keyboard.KEYCODE_DONE:
+                        break;
+                    case KEYCODE_DICTIONARY:
+                        SharedPreferenceUtil sharedPreferenceUtil = new SharedPreferenceUtil(getApplicationContext());
+
+                        break;
+                    case KEYCODE_TRANSLATION:
+                        trans_flag = !trans_flag;
+                        if (!trans_flag) {
+
+                            mInputView.setKeyboard(QwertyKeyboard);
+                        } else {
+                            mInputView.setKeyboard(TranslationKeyboard);
+                        }
+                        edit_k.getText().clear();
+
+                        break;
+                    case Keyboard.KEYCODE_MODE_CHANGE:
+                        current = mInputView.getKeyboard();
+                        if (current == TranslationKeyboard) {
+                            mInputView.setKeyboard(t_SymbolKeyboard);
+                        } else if (current == t_SymbolKeyboard || current == t_SymbolShiftKeyboard) {
+                            mInputView.setKeyboard(TranslationKeyboard);
+                        } else if (current == QwertyKeyboard) {
+                            mInputView.setKeyboard(SymbolKeyboard);
+                        } else if (current == SymbolKeyboard || current == SymbolShiftKeyboard) {
+                            mInputView.setKeyboard(QwertyKeyboard);
+                        }
+
+                        break;
+                    default:
+                        char code = (char) primaryCode;
+                        if (Character.isLetter(code) && caps) {
+                            code = Character.toUpperCase(code);
+                        }
+                        ic.commitText(String.valueOf(code), 1);
+
+                        if (mInputView.getKeyboard() == TranslationKeyboard) {
+                            prevtextlength = edit_k.getText().length();
+                            bt = edit_k.getText().toString();
+                            handletrasn(bt);
+                        }
         }
 
     }
@@ -211,59 +186,66 @@ public class SoftKeyboard extends InputMethodService implements KeyboardView.OnK
         }
     }
 
-    private class handletranslation extends AsyncTask<String,  Void, String>{
-        @Override
-        protected String doInBackground(String... strings) {
-            Log.d("test", "show");
-            String result = "";
-            String clientId = "2qfcuefDmoUdMafl8I8W";
-            String clientSecret = "";//이용시 secret 입력.
-            try {
-                Log.d("test", "I tried");
-                String text = URLEncoder.encode(strings[0], "UTF-8");
-                String apiURL = "https://openapi.naver.com/v1/language/translate";
-                URL url = new URL(apiURL);
-                HttpURLConnection con = (HttpURLConnection)url.openConnection();
-                con.setRequestMethod("POST");
-                con.setRequestProperty("X-Naver-Client-Id", clientId);
-                con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
-                // post request
-                String postParams = "source=en&target=ko&text=" + text;
-                con.setDoOutput(true);
-                DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-                wr.writeBytes(postParams);
-                wr.flush();
-                wr.close();
-                int responseCode = con.getResponseCode();
-                BufferedReader br;
-                if(responseCode==200) { // 정상 호출
-                    br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                } else {  // 에러 발생
-                    br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-                }
-                String inputLine;
-                StringBuffer response = new StringBuffer();
-                while ((inputLine = br.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                br.close();
-                result += response.toString() + "\n";
 
-                Log.d("test", response.toString());
-                //System.out.println(response.toString());
-            } catch (Exception e) {
-                Log.d("test", e.toString());
-                Log.d("test", "failed");
+    private void handletrasn(final String contents){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("test", "hihihihihihih");
+                String result = "";
+                String clientId = "2qfcuefDmoUdMafl8I8W";
+                String clientSecret = "OZag0EZWEQ";//이용시 secret 입력.
+                try {
+                    Log.d("test", "I tried");
+                    String text = URLEncoder.encode(contents, "UTF-8");
+                    String apiURL = "https://openapi.naver.com/v1/language/translate";
+                    URL url = new URL(apiURL);
+                    HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                    con.setRequestMethod("POST");
+                    con.setRequestProperty("X-Naver-Client-Id", clientId);
+                    con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+                    // post request
+                    String postParams = "source=en&target=ko&text=" + text;
+                    con.setDoOutput(true);
+                    DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+                    wr.writeBytes(postParams);
+                    wr.flush();
+                    wr.close();
+                    int responseCode = con.getResponseCode();
+                    BufferedReader br;
+                    if(responseCode==200) { // 정상 호출
+                        br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    } else {  // 에러 발생
+                        br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                    }
+                    String inputLine;
+                    StringBuffer response = new StringBuffer();
+                    while ((inputLine = br.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    br.close();
+                    String res = response.toString() + "\n";
+
+                    JSONObject resj = new JSONObject(res);
+                    res = resj.get("message").toString();
+                    resj = new JSONObject(res);
+                    res = resj.get("result").toString();
+                    resj = new JSONObject(res);
+                    res = resj.get("translatedText").toString();
+                    Log.d("test", response.toString());
+                    tent = res;
+                    getCurrentInputConnection().commitText(tent.toString(), 1);
+                    getCurrentInputConnection().setSelection(0, tent.length());
+                    prevCurrentlength = tent.length();
+                    //System.out.println(response.toString());
+                } catch (Exception e) {
+                    Log.d("test", e.toString());
+                    Log.d("test", "failed");
+                }
             }
-
-            return result;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-        }
+        }).start();
     }
+
     @Override
     public void onPress(int primaryCode) {
 
