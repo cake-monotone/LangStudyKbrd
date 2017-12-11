80292abb7db96fd144a881e11ec04743bc6c1a62
Grapefruitsaft <und37d0g@gmail.com>
2017-12-11 21:30:04 +0900

    Notification 일단 추가.

--- app/src/main/java/com/example/jms10/langstudykbrd/Notification/NotificationService.java
+++ app/src/main/java/com/example/jms10/langstudykbrd/Notification/NotificationService.java
@@ -1,5 +1,6 @@
 package com.example.jms10.langstudykbrd.Notification;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -7,6 +8,14 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+import com.example.jms10.langstudykbrd.BaseLibrary.DataFromSQL.PushNotiData.PushNotiData;
+import com.example.jms10.langstudykbrd.BaseLibrary.DataFromSQL.PushNotiData.SQLPushNotiHelper;
+import com.example.jms10.langstudykbrd.R;
+
+import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by Sojeong Jin on 11/12/2017.
@@ -14,19 +23,54 @@ import android.support.annotation.Nullable;
 
 public class NotificationService extends Service {
 
+    Context context = this;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
 
-    public void Notify(Context context){
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        Notify();
+        return START_STICKY;
+    }
+
+    public void Notify(){
         Thread t1 = new Thread(new Runnable() {
             public void run()
             {
                while(true){
                    h.sendEmptyMessageDelayed(0, 1000*60*30);
-                   cheking();
+                   SQLPushNotiHelper helper = new SQLPushNotiHelper(context);
+                   helper.open();
+                   List<PushNotiData> list = helper.getCurretDateNotices(Calendar.getInstance().getTime());
+
+                   if(list.size() == 0){
+                       helper.close();continue;
+                   }
+
+                   for(PushNotiData a : list){
+
+                       //NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+                       int icon = R.drawable.language;
+                       CharSequence notiText = "NotoManager";
+
+                       Notification notification = new  NotificationCompat.Builder(context)
+                               .setContentTitle(a.getWord())
+                               .setContentText(a.getMeaning())
+                               .setSmallIcon(icon)
+                               .build();
+
+                       /*Intent notificationIntent = new Intent(context, NotificationService.class);
+                       PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);*/
+                       notification.notify();
+
+                   }
+                   helper.close();
                }
             }});
         t1.start();
@@ -39,5 +83,5 @@ public class NotificationService extends Service {
 
             super.handleMessage(msg);
         }
-    }
+    };
 }
